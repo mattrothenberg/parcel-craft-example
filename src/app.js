7202688dafc8d44e93f813fddd43cb25a5707f5a
Matt Rothenberg <mrothenberg@heroku.com>
2019-03-15 15:45:03 -0400

    Add code splitting example

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import './app.css';
+
+const mapOfBundles = {
+  codeSplitting: import('./codeSplitting.js')
+}
+
+window.addEventListener("load", function(event) {
+  const [bodyClass] = document.body.classList
+  if (mapOfBundles[bodyClass]) {
+    mapOfBundles[bodyClass].then(res => {
+      console.log('You did it!', res)
+    })
+  }
+});
